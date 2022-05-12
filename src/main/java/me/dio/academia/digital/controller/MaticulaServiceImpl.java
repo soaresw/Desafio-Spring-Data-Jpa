8ab04f4d7809a924273ade34f7211e93839d00aa
soaresw <soares_w@hotmail.com>
2022-05-12 12:42:21 -0300

    Entrega de Projeto

---
+++ src/main/java/me/dio/academia/digital/controller/MaticulaServiceImpl.java
@@ -0,0 +1,11 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Matricula;
+
+import java.util.List;
+
+public class MaticulaServiceImpl {
+    public List<Matricula> getAll(String bairro) {
+        return null;
+    }
+}
---
+++ src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -0,0 +1,9 @@
+package me.dio.academia.digital.repository;
+
+import me.dio.academia.digital.entity.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+    List<Matricula> findByAlunoBairro(String bairro);
+
+}
